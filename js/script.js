7d04bc20eacf3e9ce980713a2e9d2773718fe7b3
josue <jmcure2@gmail.com>
2024-09-11 21:02:17 -0500

    act2.1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ function mostrar_menu(){
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
 }
+
+window.addEventListener("scroll", function(){
+    const header = document.querySelector(".menu");
+    header.classList.toggle("animacion", window.scrollY > 0);
+
+})
+
